7fe72be7db1b47aaf540537099eb5d4e0a169e8e
davidakinfenwa <davidkayode50@gmail.com>
2023-02-16 13:15:27 +0100

    Crud Function is created

--- base/views.py
+++ base/views.py
@@ -1,16 +1,45 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView,CreateView,UpdateView,DeleteView
 from .models import Task
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
 
-class Tasklisk(ListView):
+class CustomLoginView(LoginView):
+    template_name='base/login.html'
+    fields='__all__'
+    redirect_authenticated_user=True
+    
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+
+class Tasklisk(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks'
 
 
-class TaskDetail(DetailView):
+class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name='task'
     template_name='base/task.html'
+
+
+class TaskCreate(LoginRequiredMixin, CreateView):
+    model=Task
+    fields='__all__'
+    success_url=reverse_lazy('tasks')
+
+class TaskUpdate(LoginRequiredMixin, UpdateView):
+    model=Task
+    fields='__all__'
+    success_url=reverse_lazy('tasks')
+
+
+
+class TaskDelete(LoginRequiredMixin, DeleteView):
+    model=Task
+    context_object_name='tasks'
+    success_url=reverse_lazy('tasks')
